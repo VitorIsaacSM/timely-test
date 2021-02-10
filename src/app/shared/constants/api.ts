37e7b99f4de58f9eb80cbeea66da2ba30bf1987d
Vitor Maia <vitor2001bidi@gmail.com>
2021-02-10 01:01:44 -0300

    Initial architecture

---
+++ src/app/shared/constants/api.ts
@@ -0,0 +1,6 @@
+import { environment } from 'src/environments/environment';
+
+export const API = {
+  EVENTS: `${environment.apiUrl}/events`,
+  TAXONOMIES: `${environment.apiUrl}/taxonomies`,
+};
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  apiUrl: 'https://timelyapp.time.ly/api/calendars/4243455'
 };
