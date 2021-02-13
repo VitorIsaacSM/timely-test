6152610ac24b843e340fe38e541bc282510522c8
Vitor Maia <vitor2001bidi@gmail.com>
2021-02-13 14:25:23 -0300

    Added core module and header component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,15 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HeaderComponent } from './components/header/header.component';
 import { CalendarEventComponent } from './components/calendar-event/calendar-event.component';
 
 
 
 @NgModule({
-  declarations: [HeaderComponent, CalendarEventComponent],
+  declarations: [CalendarEventComponent],
   imports: [
     CommonModule
   ],
-  exports: [HeaderComponent, CalendarEventComponent]
+  exports: [CalendarEventComponent]
 })
 export class SharedModule { }
