185da9b1189a9a17a09c82367c46b8380a8bc73b
Vitor Maia <vitor2001bidi@gmail.com>
2021-02-14 18:48:46 -0300

    Add pagination component

---
+++ src/app/shared/components/pagination/pagination.component.ts
@@ -0,0 +1,29 @@
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-pagination',
+  templateUrl: './pagination.component.html',
+  styleUrls: ['./pagination.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class PaginationComponent {
+  @Input() currentPage: number;
+  @Input() totalPages: number;
+  @Output() eventChangePage = new EventEmitter<number>();
+
+  emit(page: number) {
+    this.eventChangePage.emit(page);
+  }
+  next() {
+    if (this.currentPage === this.totalPages) {
+      return;
+    }
+    this.emit(this.currentPage + 1);
+  }
+  prev() {
+    if (this.currentPage === 1) {
+      return;
+    }
+    this.emit(this.currentPage - 1);
+  }
+}
