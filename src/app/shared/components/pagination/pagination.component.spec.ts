56a50e6c0e1dff8d1bb163e6a676c6995bc6b905
Vitor Maia <vitor2001bidi@gmail.com>
2021-02-15 00:07:49 -0300

    Add unit tests for the pagination component

--- src/app/shared/components/pagination/pagination.component.spec.ts
+++ src/app/shared/components/pagination/pagination.component.spec.ts
@@ -1,10 +1,13 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { of } from 'rxjs';
 
 import { PaginationComponent } from './pagination.component';
 
 describe('PaginationComponent', () => {
   let component: PaginationComponent;
   let fixture: ComponentFixture<PaginationComponent>;
+  let spy = {};
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -16,6 +19,9 @@ describe('PaginationComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(PaginationComponent);
     component = fixture.componentInstance;
+    component.currentPage = 2;
+    component.totalPages = 10;
+    spy = spyOn(component, 'emit');
     fixture.detectChanges();
   });
 
@@ -23,7 +29,39 @@ describe('PaginationComponent', () => {
     expect(component).toBeTruthy();
   });
 
-  // emit next / prev
-  // emit last / first
-  // try to emit disabled
+  it('should emit the next page number', () => {
+    const btn = fixture.debugElement.query(By.css('.fa-angle-right'));
+    btn.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(3);
+  });
+
+  it('should emit the prev page number', () => {
+    const btn = fixture.debugElement.query(By.css('.fa-angle-left'));
+    btn.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(1);
+  });
+
+  it('should not emit the prev page number', () => {
+    component.currentPage = 1;
+    const btn = fixture.debugElement.query(By.css('.fa-angle-left'));
+    btn.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).not.toHaveBeenCalled();
+  });
+
+  it('should emit the last page number', () => {
+    const btn = fixture.debugElement.query(By.css('.fa-angle-double-right'));
+    btn.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(10);
+  });
+
+  it('should emit the first page number', () => {
+    const btn = fixture.debugElement.query(By.css('.fa-angle-double-left'));
+    btn.nativeElement.click();
+    fixture.detectChanges();
+    expect(spy).toHaveBeenCalledWith(1);
+  });
 });
