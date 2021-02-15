b148047dd428656a573dccbfa69ea3f0d10ef403
Vitor Maia <vitor2001bidi@gmail.com>
2021-02-15 02:51:34 -0300

    Add filter service unit tests

--- src/app/shared/services/filters/filters.service.spec.ts
+++ src/app/shared/services/filters/filters.service.spec.ts
@@ -1,15 +1,51 @@
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { API } from '../../constants/api';
+import { ResponseObject } from '../../models/response';
+import { FiltersService, Taxonomy, TaxonomyFilters } from './filters.service';
 
-import { FiltersService } from './filters.service';
+const FILTERED_TAXONOMIES_MOCK: TaxonomyFilters = {
+  categories: [{ id: 1, title: 'item1' }],
+  tags: []
+};
+
+const RAW_TAXONOMIES_MOCK: ResponseObject<Taxonomy[]> = {
+  code: 200,
+  data: [
+    {
+      id: 1,
+      items: [{ id: 1, title: 'item1' }],
+      name: 'taxonomy_category',
+      query_name: 'taxonomy_category'
+    }
+  ],
+  message: '',
+  success: true
+};
 
 describe('FiltersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({
-    imports: [HttpClientModule]
-  }));
+  let service: FiltersService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    service = TestBed.get(FiltersService);
+    httpMock = TestBed.get(HttpTestingController);
+  });
 
   it('should be created', () => {
-    const service: FiltersService = TestBed.get(FiltersService);
     expect(service).toBeTruthy();
+  });
+
+  it('should return a filtered list of taxonomies', () => {
+    service.taxonomies.subscribe(res => {
+      expect(res.data).toEqual(FILTERED_TAXONOMIES_MOCK);
+    });
+
+    const req = httpMock.expectOne(API.TAXONOMIES);
+    expect(req.request.method).toBe('GET');
+    req.flush(RAW_TAXONOMIES_MOCK);
   });
 });
